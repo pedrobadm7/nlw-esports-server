9961948e0b67d14503033360bafbe665175eacbe
Pedro Barros <pedrobars7a@gmail.com>
2022-09-14 08:18:59 -0300

    instal prettier

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        singleQuote: true,
+        semi: true,
+      },
+    ],
+  },
+};
