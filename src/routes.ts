090d0637811ec7a3c58c97fc024da5563157c116
Pedro Barros <pedrobars7a@gmail.com>
2022-09-14 12:14:08 -0300

    Aula 03

---
+++ src/routes.ts
@@ -0,0 +1,96 @@
+import Router, { Request, Response } from 'express';
+
+const router = Router();
+import { PrismaClient } from '@prisma/client';
+import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes';
+import { convertMinutesToHourString } from './utils/convert-minutes-to-hour-string';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
+
+router.get('/games', async (request: Request, response: Response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+
+  return response.json(games);
+});
+
+router.post('/games/:id/ads', async (request: Request, response: Response) => {
+  const gameId = request.params.id;
+  const body: any = request.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(','),
+      hourStart: convertHourStringToMinutes(body.hourStart),
+      hourEnd: convertHourStringToMinutes(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(ad);
+});
+
+router.get('/games/:id/ads', async (request: Request, response: Response) => {
+  const gameId = request.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return response.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(','),
+        hourStart: convertMinutesToHourString(ad.hourStart),
+        hourEnd: convertMinutesToHourString(ad.hourEnd),
+      };
+    })
+  );
+});
+
+router.get('/ads/:id/discord', async (request: Request, response: Response) => {
+  const adId = request.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return response.json({
+    discord: ad.discord,
+  });
+});
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import router from './routes';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(router);
 
-app.listen(3333);
+app.listen(3333, () => console.log('Server is running on port 3333 🔥'));
