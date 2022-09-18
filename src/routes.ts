143f13969e33ab912a42e74cd2e1cce010b0daff
Pedro Barros <pedrobars7a@gmail.com>
2022-09-18 06:34:40 -0300

    format

--- src/routes.ts
+++ src/routes.ts
@@ -9,17 +9,12 @@ import { UsersController } from './controllers/UsersController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
 router.get('/games', isAuthenticated, new GamesController().index);
-
 router.get('/games/:id/ads', isAuthenticated, new AdsController().show);
-
 router.get('/profile', isAuthenticated, new UsersController().index);
-
 router.get('/ads/:id/discord', isAuthenticated, new DiscordController().show);
 
 router.post('/games/:id/ads', isAuthenticated, new AdsController().store);
-
 router.post('/auth/register', new AuthController().auth);
-
 router.post('/login', new AuthController().login);
 
 export default router;
